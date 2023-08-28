bb85fe0c12bca02e0bd93370954689dbbbe6510d
Marina Menezes <marinamenezes@me.com>
2023-08-28 13:05:36 -0300

    Adiciona gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
-
+const image = require('gulp-imagemin')
 
 function tarefasCSS(cb) {
 
     return gulp.src('./vendor/**/*.css')
         .pipe(concat('libs.css'))
         .pipe(cssmin())
-        .pipe(rename({suffix: '.min'})) //libs.min.css
+        .pipe(rename({ suffix: '.min'})) // libs.min.css
         .pipe(gulp.dest('./dist/css'))
 
 }
@@ -20,10 +20,28 @@ function tarefasJS(){
     return gulp.src('./vendor/**/*.js')
         .pipe(concat('libs.js'))
         .pipe(uglify())
-        .pipe(rename({suffix: '.min'})) //libs.min.js
+        .pipe(rename({ suffix: '.min'})) //libs.min.js
         .pipe(gulp.dest('./dist/js'))
+}
 
+
+function tarefasImagem(){
+    
+    return gulp.src('./src/images/*')
+        .pipe(image({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true
+        }))
+        .pipe(gulp.dest('./dist/images'))
 }
 
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
+exports.images = tarefasImagem
